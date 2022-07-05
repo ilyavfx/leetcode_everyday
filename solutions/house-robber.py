a11b30c69c0d0da50534ede52b0123cabf429f3e
Ilia Suvorkin <ilyafx@gmail.com>
2022-07-05 13:16:57 +0300

    house robber

---
+++ solutions/house-robber.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/house-robber/
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def dp(i: int):
+            if i == 0:
+                return nums[0]
+            if i == 1:
+                return max(nums[0], nums[1])
+            if i not in memo:
+                memo[i] = max(dp(i - 1), dp(i - 2) + nums[i])
+
+            return memo[i]
+
+        memo = {}
+        return dp(len(nums) - 1)
